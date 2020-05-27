aa51a78e1ef16b4bb274ed75614f5da5082d5ee6
Vadym <volkov.vadym@gmail.com>
2020-05-27 17:40:13 +0300

    Initial Commit

---
+++ HomeWork7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork7
+//
+//  Created by Vadym Volkov on 27.05.2020.
+//  Copyright © 2020 Vadym Volkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
